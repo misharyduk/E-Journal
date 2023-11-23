8a7faa4a108d139debc6b5232164b5e948ccd185
misharyduk <misharyduk64@gmail.com>
2023-11-23 20:42:13 +0200

    added CRUD REST endpoints related to subjects

--- university/src/main/java/com/ejournal/university/controller/SubjectController.java
+++ university/src/main/java/com/ejournal/university/controller/SubjectController.java
@@ -1,9 +1,54 @@
 package com.ejournal.university.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ejournal.university.dto.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/subjects")
 public class SubjectController {
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createSubject(@RequestBody SubjectRequestDto subjectRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(HttpStatus.CREATED.toString(), "Subject has been successfully created"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SubjectResponseDto>> fetchAllSubjects(){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of());
+    }
+
+    @GetMapping("{subjectId}")
+    public ResponseEntity<SubjectResponseDto> fetchSubject(@PathVariable("subjectId") Integer subjectId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new SubjectResponseDto());
+    }
+
+    @PutMapping
+    public ResponseEntity<SubjectResponseDto> updateSubject(@RequestBody SubjectRequestDto subjectRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new SubjectResponseDto());
+    }
+
+    @DeleteMapping("{subjectId}")
+    public ResponseEntity<ResponseDto> deleteSubject(@PathVariable("subjectId") Integer subjectId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(HttpStatus.OK.toString(), "Subject has been successfully created"));
+    }
+
 }
