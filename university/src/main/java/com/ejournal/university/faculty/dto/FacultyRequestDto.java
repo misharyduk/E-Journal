fc5304710e682cda4cec401627edc41fe146b1c9
misharyduk <misharyduk64@gmail.com>
2023-11-23 21:48:43 +0200

    added details to request about faculty

--- university/src/main/java/com/ejournal/university/faculty/dto/FacultyRequestDto.java
+++ university/src/main/java/com/ejournal/university/faculty/dto/FacultyRequestDto.java
@@ -1,4 +1,22 @@
 package com.ejournal.university.faculty.dto;
 
+import com.ejournal.university.info.dto.UniversityRequestDto;
+
 public class FacultyRequestDto {
+
+    private String facultyName;
+    private String facultyDescription;
+    private AddressDto address;
+    private String mobilePhone;
+    private String email;
+    private Long deanId;
+
+    private class AddressDto{
+        private String country;
+        private String city;
+        private String street;
+        private String number;
+        private String zipCode;
+        private String officeNumber;
+    }
 }
