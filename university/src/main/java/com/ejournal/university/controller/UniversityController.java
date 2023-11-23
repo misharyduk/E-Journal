35540d55224764bf25a5ddb9c2f3c00746cea4fc
misharyduk <misharyduk64@gmail.com>
2023-11-23 20:16:38 +0200

    change request mapping for university controller

--- university/src/main/java/com/ejournal/university/controller/UniversityController.java
+++ university/src/main/java/com/ejournal/university/controller/UniversityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/university/")
 public class UniversityController {
 
     @PostMapping
