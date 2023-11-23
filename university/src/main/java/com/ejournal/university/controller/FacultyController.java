69626046ee467f751ce403258b73915b24b9ee02
misharyduk <misharyduk64@gmail.com>
2023-11-23 20:30:05 +0200

    added CRUD REST endpoints related to faculty

--- university/src/main/java/com/ejournal/university/controller/FacultyController.java
+++ university/src/main/java/com/ejournal/university/controller/FacultyController.java
@@ -1,9 +1,56 @@
 package com.ejournal.university.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ejournal.university.dto.FacultyRequestDto;
+import com.ejournal.university.dto.FacultyResponseDto;
+import com.ejournal.university.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/faculties/")
 public class FacultyController {
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createFaculty(@RequestBody FacultyRequestDto facultyRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(HttpStatus.CREATED.toString(), "Faculty has been successfully created"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FacultyResponseDto>> fetchAllFaculties(){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of());
+    }
+
+    @GetMapping("{facultyId}")
+    public ResponseEntity<FacultyResponseDto> fetchFaculty(@PathVariable("facultyId") Integer facultyId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new FacultyResponseDto());
+    }
+
+    @PutMapping
+    public ResponseEntity<FacultyResponseDto> updateFaculty(@RequestBody FacultyRequestDto facultyRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new FacultyResponseDto());
+    }
+
+    @DeleteMapping("{facultyId}")
+    public ResponseEntity<ResponseDto> deleteFaculty(@PathVariable("facultyId") Integer facultyId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(HttpStatus.OK.toString(), "Faculty has been successfully created"));
+    }
+
 }
