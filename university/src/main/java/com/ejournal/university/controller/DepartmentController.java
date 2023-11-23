304d67992d0bd12aafc68f17a64c5f7e3a1fb4e1
misharyduk <misharyduk64@gmail.com>
2023-11-23 20:37:19 +0200

    added CRUD REST endpoints related to departments

--- university/src/main/java/com/ejournal/university/controller/DepartmentController.java
+++ university/src/main/java/com/ejournal/university/controller/DepartmentController.java
@@ -1,9 +1,56 @@
 package com.ejournal.university.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ejournal.university.dto.DepartmentRequestDto;
+import com.ejournal.university.dto.DepartmentResponseDto;
+import com.ejournal.university.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/departments")
 public class DepartmentController {
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createDepartment(@RequestBody DepartmentRequestDto departmentRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(HttpStatus.CREATED.toString(), "Department has been successfully created"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentResponseDto>> fetchAllDepartments(){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(List.of());
+    }
+
+    @GetMapping("/{departmentId}")
+    public ResponseEntity<DepartmentResponseDto> fetchDepartment(@PathVariable("departmentId") Integer departmentId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new DepartmentResponseDto());
+    }
+
+    @PutMapping
+    public ResponseEntity<DepartmentResponseDto> updateDepartment(@RequestBody DepartmentRequestDto departmentRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new DepartmentResponseDto());
+    }
+
+    @DeleteMapping("/{departmentId}")
+    public ResponseEntity<ResponseDto> deleteDepartment(@PathVariable("departmentId") Integer departmentId){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(HttpStatus.OK.toString(), "Department has been successfully deleted"));
+    }
+
 }
