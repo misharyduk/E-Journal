3f4189ae23d2f11b3c96e9ea153578c019b7bc82
misharyduk <misharyduk64@gmail.com>
2023-11-23 21:05:50 +0200

    added basic details about university DTO

--- university/src/main/java/com/ejournal/university/info/dto/UniversityRequestDto.java
+++ university/src/main/java/com/ejournal/university/info/dto/UniversityRequestDto.java
@@ -1,4 +1,26 @@
 package com.ejournal.university.info.dto;
 
 public class UniversityRequestDto {
+
+    private String universityName;
+    private String universityDescription;
+    private AddressDto address;
+    private String mobilePhone;
+    private String accreditation;
+    private Rector rector;
+
+    private class AddressDto{
+        private String country;
+        private String city;
+        private String street;
+        private String number;
+        private String zipCode;
+    }
+
+    private class Rector{
+        private String firstName;
+        private String lastName;
+        private String middleName;
+    }
+
 }
