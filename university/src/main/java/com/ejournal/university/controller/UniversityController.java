e7841fdd312f429faa6e3ddf3cfe5ed2535e74e2
misharyduk <misharyduk64@gmail.com>
2023-11-23 17:18:24 +0200

    added endpoint for creating

--- university/src/main/java/com/ejournal/university/controller/UniversityController.java
+++ university/src/main/java/com/ejournal/university/controller/UniversityController.java
@@ -1,9 +1,24 @@
 package com.ejournal.university.controller;
 
+import com.ejournal.university.dto.ResponseDto;
+import com.ejournal.university.dto.UniversityRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class UniversityController {
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createUniversity(@RequestBody UniversityRequestDto universityRequestDto){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(HttpStatus.CREATED.toString(), "University has been successfully created"));
+    }
+
 }
