8f3e2feacb7463de60e46ed64e8c6c429a313822
misharyduk <misharyduk64@gmail.com>
2023-11-24 12:58:28 +0200

    added response fields

--- university/src/main/java/com/ejournal/university/faculty/dto/FacultyResponseDto.java
+++ university/src/main/java/com/ejournal/university/faculty/dto/FacultyResponseDto.java
@@ -1,4 +1,15 @@
 package com.ejournal.university.faculty.dto;
 
+import com.ejournal.university.common.dto.AddressDto;
+
 public class FacultyResponseDto {
+
+    private String facultyName;
+    private String facultyDescription;
+    private AddressDto address;
+    private String officeNumber;
+    private String mobilePhone;
+    private String email;
+    private Long deanId;
+
 }
