4a8bfb520b64d68fda545594d0447aeb3ea37052
misharyduk <misharyduk64@gmail.com>
2023-11-24 15:04:30 +0200

    added fake implementation for service

--- university/src/main/java/com/ejournal/university/info/service/impl/UniversityServiceImpl.java
+++ university/src/main/java/com/ejournal/university/info/service/impl/UniversityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ejournal.university.info.service.impl;
 
+import com.ejournal.university.common.dto.AddressDto;
 import com.ejournal.university.info.dto.UniversityDto;
 import com.ejournal.university.info.service.UniversityService;
 import org.springframework.stereotype.Service;
@@ -7,25 +8,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class UniversityServiceImpl implements UniversityService {
 
-    private static final UniversityDto UNIVERSITY = new UniversityDto();
+    private static final UniversityDto UNIVERSITY = UniversityDto.builder()
+            .setUniversityName("Національний Авіаційний Університет")
+            .setUniversityDescription("Національний авіаційний університет – один " +
+                    "із найпотужніших авіаційних закладів вищої освіти у світі, в " +
+                    "якому навчається близько 25 тисячі студентів, серед яких майже " +
+                    "1500 іноземців з 55 країн світу. Ректор університету – доктор " +
+                    "технічних наук, професор, Луцький Максим Георгійович")
+            .setAddress(new AddressDto("Україна", "Київ", "пр. Гузара Любомира", "1", "03058"))
+            .setEmail("post@nau.edu.ua")
+            .setMobilePhone("(044) 406-79-01")
+            .setAccreditation("4")
+            .setRector(new UniversityDto.RectorDto("Максим", "Луцький", "Георгійович"))
+            .build();
 
     @Override
     public boolean createUniversity(UniversityDto universityRequestDto) {
-        return false;
+        return true;
     }
 
     @Override
     public UniversityDto fetchUniversityDetails() {
-        return null;
+        return UNIVERSITY;
     }
 
     @Override
     public UniversityDto updateUniversityDetails(UniversityDto universityRequestDto) {
-        return null;
+        return UNIVERSITY;
     }
 
     @Override
     public boolean deleteUniversity() {
-        return false;
+        return true;
     }
 }
