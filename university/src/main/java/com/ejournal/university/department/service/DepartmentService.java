8e27eba59c4b12234b6ada15e6b9eff6bf36ef45
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:38:14 +0200

    added CRUD method for department service interface

--- university/src/main/java/com/ejournal/university/department/service/DepartmentService.java
+++ university/src/main/java/com/ejournal/university/department/service/DepartmentService.java
@@ -1,11 +1,19 @@
 package com.ejournal.university.department.service;
 
-import com.ejournal.university.faculty.dto.FacultyRequestDto;
-import com.ejournal.university.faculty.dto.FacultyResponseDto;
+import com.ejournal.university.department.dto.DepartmentRequestDto;
+import com.ejournal.university.department.dto.DepartmentResponseDto;
 
 import java.util.List;
 
 public interface DepartmentService {
 
+    boolean createDepartment(DepartmentRequestDto departmentRequestDto);
 
+    List<DepartmentResponseDto> fetchAllDepartments();
+
+    DepartmentResponseDto fetchDepartment(Integer departmentId);
+
+    DepartmentResponseDto updateDepartment(DepartmentRequestDto departmentRequestDto);
+
+    boolean deleteDepartment(Integer departmentId);
 }
