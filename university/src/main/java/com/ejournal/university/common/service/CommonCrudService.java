6f9cf1265fe34f27102b42a527dc8936a23e3d29
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:51:42 +0200

    added common service interface with basic CRUD operations

---
+++ university/src/main/java/com/ejournal/university/common/service/CommonCrudService.java
@@ -0,0 +1,20 @@
+package com.ejournal.university.common.service;
+
+import com.ejournal.university.info.dto.UniversityRequestDto;
+import com.ejournal.university.info.dto.UniversityResponseDto;
+
+import java.util.List;
+
+public interface CommonCrudService<K1, K2> {
+
+    boolean create(K1 requestDto);
+
+    K2 fetchById(Long id);
+
+    List<K2> fetchAll();
+
+    K2 update(K1 requestDto);
+
+    boolean deleteById(Long id);
+
+}
