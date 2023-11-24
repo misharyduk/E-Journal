7bfc5f833be1e7e857a1f76068bf3aa7155c43d6
misharyduk <misharyduk64@gmail.com>
2023-11-24 13:00:38 +0200

    added request fields

--- university/src/main/java/com/ejournal/university/department/dto/DepartmentRequestDto.java
+++ university/src/main/java/com/ejournal/university/department/dto/DepartmentRequestDto.java
@@ -1,4 +1,15 @@
 package com.ejournal.university.department.dto;
 
+import com.ejournal.university.common.dto.AddressDto;
+
 public class DepartmentRequestDto {
+
+    private String departmentName;
+    private String departmentDescription;
+    private AddressDto address;
+    private String officeNumber;
+    private String mobilePhone;
+    private String email;
+    private Long headOfDepartment;
+
 }
