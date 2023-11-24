60e3f691e22c5f11683eadeb0d8618aa79b07949
misharyduk <misharyduk64@gmail.com>
2023-11-24 16:23:16 +0200

    added constants related to university microservice

---
+++ university/src/main/java/com/ejournal/university/common/util/ServiceConstants.java
@@ -0,0 +1,22 @@
+package com.ejournal.university.common.util;
+
+public class ServiceConstants {
+
+    // STATUS CODE CONSTANTS
+    public static final String STATUS_CODE_200 = "200";
+    public static final String STATUS_MESSAGE_200 = "Request has been processed successfully";
+    public static final String STATUS_CODE_201 = "201";
+    public static final String STATUS_MESSAGE_201 = " has been created successfully";
+    public static final String STATUS_CODE_404 = "404";
+    public static final String STATUS_MESSAGE_404 = " cannot be found";
+    public static final String STATUS_CODE_417 = "417";
+    public static final String STATUS_MESSAGE_417 = "Requested operation failed";
+    public static final String STATUS_MESSAGE_417_UPDATE = "Update operation failed";
+    public static final String STATUS_MESSAGE_417_DELETE = "Delete operation failed";
+    public static final String STATUS_CODE_500 = "500";
+    public static final String STATUS_MESSAGE_500 = "An error occurred. Wait for a while and try again";
+
+    // BUSINESS LOGIC CONSTANTS
+    public static final String INSTANCE_DELETED = " has been deleted successfully";
+
+}
