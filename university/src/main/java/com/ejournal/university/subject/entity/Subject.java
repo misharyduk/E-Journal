5a67bd8115925563423000a22229e71ea3317ba0
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:10:52 +0200

    added JPA annotations

--- university/src/main/java/com/ejournal/university/subject/entity/Subject.java
+++ university/src/main/java/com/ejournal/university/subject/entity/Subject.java
@@ -1,12 +1,18 @@
 package com.ejournal.university.subject.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter @Setter
 public class Subject {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subject_id")
     private Long id;
+    @Column(name = "subject_name")
     private String name;
 
 }
