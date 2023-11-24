62f055e6c88a0ca294c766cec109236ec331d076
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:21:35 +0200

    added JPA annotations for Department entity

--- university/src/main/java/com/ejournal/university/department/entity/Department.java
+++ university/src/main/java/com/ejournal/university/department/entity/Department.java
@@ -5,20 +5,38 @@ import com.ejournal.university.faculty.entity.Faculty;
 import com.ejournal.university.info.entity.University;
 import com.ejournal.university.teacher.entity.Teacher;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Getter @Setter
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "faculty_id")
     private Long id;
+    @Column(name = "department_name")
     private String departmentName;
+    @Column(name = "department_description")
     private String departmentDescription;
+    @OneToOne
+    @JoinColumn(name = "address_id")
     private Address address;
+    @Column(name = "office_number")
     private String officeNumber;
+    @Column(name = "mobile_phone")
     private String mobilePhone;
+    @Column(name = "email")
     private String email;
+    @OneToOne
+    @JoinColumn(name = "head_of_department_id")
     private Teacher headOfDepartment;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id")
     private Faculty faculty;
 
 }
