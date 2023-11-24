648dd9650d82de4e3d8dfc9c7f5688bd6c7ca2aa
misharyduk <misharyduk64@gmail.com>
2023-11-24 16:23:36 +0200

    inject service and delegate logic on it

--- university/src/main/java/com/ejournal/university/info/controller/UniversityController.java
+++ university/src/main/java/com/ejournal/university/info/controller/UniversityController.java
@@ -2,44 +2,55 @@ package com.ejournal.university.info.controller;
 
 import com.ejournal.university.common.dto.ResponseDto;
 import com.ejournal.university.info.dto.UniversityDto;
+import com.ejournal.university.info.service.UniversityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.ejournal.university.common.util.ServiceConstants.*;
+
 @RestController
 @RequestMapping("/api/v1/university")
+@RequiredArgsConstructor
 public class UniversityController {
+
+    private final UniversityService universityService;
 
     @PostMapping
     public ResponseEntity<ResponseDto> createUniversity(@RequestBody UniversityDto universityRequestDto){
 
+        universityService.createUniversity(universityRequestDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new ResponseDto(HttpStatus.CREATED.toString(), "University has been successfully created"));
+                .body(new ResponseDto(STATUS_CODE_201, "University" + STATUS_MESSAGE_201));
     }
 
     @GetMapping
     public ResponseEntity<UniversityDto> fetchUniversityDetails(){
 
+        UniversityDto universityDto = universityService.fetchUniversityDetails();
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new UniversityDto());
+                .body(universityDto);
     }
 
     @PutMapping
     public ResponseEntity<UniversityDto> updateUniversityDetails(@RequestBody UniversityDto universityRequestDto){
 
+        UniversityDto updatedUniversityDto = universityService.updateUniversityDetails(universityRequestDto);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new UniversityDto());
+                .body(updatedUniversityDto);
     }
 
     @DeleteMapping
     public ResponseEntity<ResponseDto> deleteUniversity(){
 
+        universityService.deleteUniversity();
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ResponseDto(HttpStatus.OK.toString(), "University has been successfully deleted"));
+                .body(new ResponseDto(HttpStatus.OK.toString(), "University" + INSTANCE_DELETED));
     }
 
 }
