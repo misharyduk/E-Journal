e9c4dd52235ec4244e18128bbcb78145404c958c
misharyduk <misharyduk64@gmail.com>
2023-11-24 13:07:21 +0200

    added request fields

--- university/src/main/java/com/ejournal/university/teacher/dto/TeacherRequestDto.java
+++ university/src/main/java/com/ejournal/university/teacher/dto/TeacherRequestDto.java
@@ -1,4 +1,18 @@
 package com.ejournal.university.teacher.dto;
 
+import com.ejournal.university.common.dto.AddressDto;
+import com.ejournal.university.teacher.entity.AcademicRank;
+
+import java.util.List;
+
 public class TeacherRequestDto {
+
+    private String firstName;
+    private String secondName;
+    private String middleName;
+    private List<AcademicRank> academicRanks;
+    private AddressDto homeAddress;
+    private String mobilePhone;
+    private String email;
+
 }
