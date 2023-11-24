61d50d6f21faf07caab8278164dd7ab3ba98ba7a
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:57:28 +0200

    init service interface for subject and teacher

---
+++ university/src/main/java/com/ejournal/university/subject/service/SubjectService.java
@@ -0,0 +1,9 @@
+package com.ejournal.university.subject.service;
+
+import com.ejournal.university.common.service.CommonCrudService;
+import com.ejournal.university.subject.dto.SubjectRequestDto;
+import com.ejournal.university.subject.dto.SubjectResponseDto;
+
+public interface SubjectService extends CommonCrudService<SubjectRequestDto, SubjectResponseDto> {
+
+}
---
+++ university/src/main/java/com/ejournal/university/teacher/service/TeacherService.java
@@ -0,0 +1,9 @@
+package com.ejournal.university.teacher.service;
+
+import com.ejournal.university.common.service.CommonCrudService;
+import com.ejournal.university.teacher.dto.TeacherRequestDto;
+import com.ejournal.university.teacher.dto.TeacherResponseDto;
+
+public interface TeacherService extends CommonCrudService<TeacherRequestDto, TeacherResponseDto> {
+
+}
