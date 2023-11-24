f9398a421eb66d14ff0d3404d6497d9ff7fdd92b
misharyduk <misharyduk64@gmail.com>
2023-11-24 15:03:49 +0200

    added basic fields

--- university/src/main/java/com/ejournal/university/common/dto/AddressDto.java
+++ university/src/main/java/com/ejournal/university/common/dto/AddressDto.java
@@ -1,4 +1,18 @@
 package com.ejournal.university.common.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
 public class AddressDto {
+
+    public String country;
+    public String city;
+    public String street;
+    public String number;
+    public String zipCode;
+
 }
