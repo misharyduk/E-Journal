874e02877f9b50a43a62671ef574e13fae58a9e5
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:18:29 +0200

    added fields for department entity

--- university/src/main/java/com/ejournal/university/department/entity/Department.java
+++ university/src/main/java/com/ejournal/university/department/entity/Department.java
@@ -1,4 +1,24 @@
 package com.ejournal.university.department.entity;
 
+import com.ejournal.university.common.entity.Address;
+import com.ejournal.university.faculty.entity.Faculty;
+import com.ejournal.university.info.entity.University;
+import com.ejournal.university.teacher.entity.Teacher;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
+
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private Address address;
+    private String officeNumber;
+    private String mobilePhone;
+    private String email;
+    private Teacher headOfDepartment;
+    private Faculty faculty;
+
 }
