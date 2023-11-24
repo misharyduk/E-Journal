2e387f65c21bb6656be31063d2f852f275f39aed
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:07:47 +0200

    added JPA entity

---
+++ university/src/main/java/com/ejournal/university/teacher/entity/Teacher.java
@@ -0,0 +1,28 @@
+package com.ejournal.university.teacher.entity;
+
+import com.ejournal.university.faculty.entity.Faculty;
+import jakarta.persistence.*;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "teacher_id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "middle_name")
+    private String middleName;
+    //    private List<String> academicRanks;
+    @Column(name = "mobile_phone")
+    private String mobilePhone;
+    @Column(name = "email")
+    private String email;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id")
+    private Faculty faculty;
+
+}
