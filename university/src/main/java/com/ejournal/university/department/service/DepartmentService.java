0bf68f5afa026c210e441563fd9bcaaaf7969367
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:36:05 +0200

    init interface for department service

---
+++ university/src/main/java/com/ejournal/university/department/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package com.ejournal.university.department.service;
+
+import com.ejournal.university.faculty.dto.FacultyRequestDto;
+import com.ejournal.university.faculty.dto.FacultyResponseDto;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+
+}
