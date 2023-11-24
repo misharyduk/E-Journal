84b931b42fd42b87f077438ab186886ff1481d5c
misharyduk <misharyduk64@gmail.com>
2023-11-24 18:13:07 +0200

    added university fields

--- university/src/main/java/com/ejournal/university/info/entity/University.java
+++ university/src/main/java/com/ejournal/university/info/entity/University.java
@@ -1,4 +1,23 @@
 package com.ejournal.university.info.entity;
 
+import com.ejournal.university.common.entity.Address;
+import com.ejournal.university.faculty.entity.Faculty;
+import com.ejournal.university.teacher.entity.Teacher;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class University {
+
+    private Long id;
+    private String facultyName;
+    private String facultyDescription;
+    private Address address;
+    private String mobilePhone;
+    private String email;
+    private String accreditation;
+    private Teacher rector;
+    private List<Faculty> faculties = new ArrayList<>();
+
 }
