5974fadedcda6e3402a2fc1ad7e069915d29726b
misharyduk <misharyduk64@gmail.com>
2023-11-25 10:55:47 +0200

    inject repository in department service

--- university/src/main/java/com/ejournal/university/department/service/impl/DepartmentServiceImpl.java
+++ university/src/main/java/com/ejournal/university/department/service/impl/DepartmentServiceImpl.java
@@ -2,13 +2,19 @@ package com.ejournal.university.department.service.impl;
 
 import com.ejournal.university.department.dto.DepartmentRequestDto;
 import com.ejournal.university.department.dto.DepartmentResponseDto;
+import com.ejournal.university.department.repository.DepartmentRepository;
+import com.ejournal.university.department.repository.impl.JpaDepartmentRepositoryImpl;
 import com.ejournal.university.department.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
 
     @Override
     public boolean create(DepartmentRequestDto requestDto) {
