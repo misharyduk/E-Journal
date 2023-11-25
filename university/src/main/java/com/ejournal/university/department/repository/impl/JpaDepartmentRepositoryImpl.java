7894bc7e1fee31304f3bf804f2cfae1cefa13d5a
misharyduk <misharyduk64@gmail.com>
2023-11-25 11:15:30 +0200

    added delegation based implementation of repository interface with the help of default methods

--- university/src/main/java/com/ejournal/university/department/repository/impl/JpaDepartmentRepositoryImpl.java
+++ university/src/main/java/com/ejournal/university/department/repository/impl/JpaDepartmentRepositoryImpl.java
@@ -5,6 +5,34 @@ import com.ejournal.university.department.repository.DepartmentRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface JpaDepartmentRepositoryImpl extends JpaRepository<Department, Long>, DepartmentRepository {
+
+    @Override
+    default List<Department> findAllDepartments(){
+        return findAll();
+    }
+
+    @Override
+    default Optional<Department> findDepartmentById(Long departmentId){
+        return findById(departmentId);
+    }
+
+    @Override
+    default Department createDepartment(Department department){
+        return save(department);
+    }
+
+    @Override
+    default Department updateDepartment(Department department){
+        return save(department);
+    }
+
+    @Override
+    default void deleteDepartments(Department department){
+        delete(department);
+    }
 }
