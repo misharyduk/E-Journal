01984d7f751c736b6a000c64bd5a8363591f39a3
misharyduk <misharyduk64@gmail.com>
2023-11-25 11:14:18 +0200

    added method for basic CRUD operations

--- university/src/main/java/com/ejournal/university/department/repository/DepartmentRepository.java
+++ university/src/main/java/com/ejournal/university/department/repository/DepartmentRepository.java
@@ -1,4 +1,18 @@
 package com.ejournal.university.department.repository;
 
+import com.ejournal.university.department.entity.Department;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface DepartmentRepository {
+    List<Department> findAllDepartments();
+
+    Optional<Department> findDepartmentById(Long departmentId);
+
+    Department createDepartment(Department department);
+
+    Department updateDepartment(Department department);
+
+    void deleteDepartments(Department department);
 }
