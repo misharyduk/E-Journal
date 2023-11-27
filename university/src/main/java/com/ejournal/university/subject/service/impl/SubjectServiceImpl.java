b58085bb8faf25f4ee34241578d0d778349d2bae
misharyduk <misharyduk64@gmail.com>
2023-11-27 14:32:54 +0200

    implemented basic CRUD operations for subject's service using repository

--- university/src/main/java/com/ejournal/university/subject/service/impl/SubjectServiceImpl.java
+++ university/src/main/java/com/ejournal/university/subject/service/impl/SubjectServiceImpl.java
@@ -1,34 +1,59 @@
 package com.ejournal.university.subject.service.impl;
 
+import com.ejournal.university.common.exception.ResourceNotFoundException;
 import com.ejournal.university.subject.dto.SubjectRequestDto;
 import com.ejournal.university.subject.dto.SubjectResponseDto;
+import com.ejournal.university.subject.entity.Subject;
+import com.ejournal.university.subject.mapper.SubjectMapper;
+import com.ejournal.university.subject.repository.SubjectRepository;
 import com.ejournal.university.subject.service.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class SubjectServiceImpl implements SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
     @Override
     public SubjectResponseDto create(SubjectRequestDto requestDto) {
-        return null;
+        Subject subject = SubjectMapper.mapToEntity(requestDto, new Subject());
+        return SubjectMapper.mapToDto(subjectRepository.createInstance(subject));
     }
 
     @Override
     public SubjectResponseDto fetchById(Long id) {
-        return null;
+        Subject subject = subjectRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Subject", "id", String.valueOf(id)));
+        return SubjectMapper.mapToDto(subject);
     }
 
     @Override
     public List<SubjectResponseDto> fetchAll() {
-        return null;
+        List<Subject> allSubjects = subjectRepository.fetchAllInstances();
+        return allSubjects.stream()
+                .map(SubjectMapper::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public SubjectResponseDto update(Long id, SubjectRequestDto requestDto) {
-        return null;
+        Subject subject = subjectRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Subject", "id", String.valueOf(id)));
+        Subject updatedSubject = SubjectMapper.mapToEntity(requestDto, subject);
+        updatedSubject = subjectRepository.updateInstance(updatedSubject);
+        return SubjectMapper.mapToDto(updatedSubject);
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        Subject subject = subjectRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Subject", "id", String.valueOf(id)));
+        subjectRepository.deleteInstance(subject);
+        return true;
     }
 }
