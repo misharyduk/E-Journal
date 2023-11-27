9d99932069ab4bd721c7afcbeaf88d1d2efc2876
misharyduk <misharyduk64@gmail.com>
2023-11-27 09:22:16 +0200

    implement department's service methods

--- university/src/main/java/com/ejournal/university/department/service/impl/DepartmentServiceImpl.java
+++ university/src/main/java/com/ejournal/university/department/service/impl/DepartmentServiceImpl.java
@@ -1,16 +1,17 @@
 package com.ejournal.university.department.service.impl;
 
+import com.ejournal.university.common.exception.ResourceNotFoundException;
 import com.ejournal.university.department.dto.DepartmentRequestDto;
 import com.ejournal.university.department.dto.DepartmentResponseDto;
 import com.ejournal.university.department.entity.Department;
 import com.ejournal.university.department.mapper.DepartmentMapper;
 import com.ejournal.university.department.repository.DepartmentRepository;
-import com.ejournal.university.department.repository.impl.JpaDepartmentRepositoryImpl;
 import com.ejournal.university.department.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,21 +27,33 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentResponseDto fetchById(Long id) {
-        return null;
+        Department department = departmentRepository.findDepartmentById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department", "id", String.valueOf(id)));
+        return DepartmentMapper.mapToResponseDto(department);
     }
 
     @Override
     public List<DepartmentResponseDto> fetchAll() {
-        return null;
+        List<Department> allDepartments = departmentRepository.findAllDepartments();
+        return allDepartments.stream()
+                .map(DepartmentMapper::mapToResponseDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public DepartmentResponseDto update(DepartmentRequestDto requestDto) {
-        return null;
+    public DepartmentResponseDto update(Long id, DepartmentRequestDto requestDto) {
+        Department department = departmentRepository.findDepartmentById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department", "id", String.valueOf(id)));
+        Department updatedDepartment = DepartmentMapper.mapToEntity(requestDto, department);
+        updatedDepartment = departmentRepository.updateDepartment(updatedDepartment);
+        return DepartmentMapper.mapToResponseDto(updatedDepartment);
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        Department department = departmentRepository.findDepartmentById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department", "id", String.valueOf(id)));
+        departmentRepository.deleteDepartment(department);
+        return true;
     }
 }
