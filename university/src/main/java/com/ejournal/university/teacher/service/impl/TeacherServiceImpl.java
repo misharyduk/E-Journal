3b92a7afd068a43f82c30c1a06d1b45fee6f4659
misharyduk <misharyduk64@gmail.com>
2023-11-27 14:23:19 +0200

    implemented basic CRUD operations for teacher's service using repository

--- university/src/main/java/com/ejournal/university/teacher/service/impl/TeacherServiceImpl.java
+++ university/src/main/java/com/ejournal/university/teacher/service/impl/TeacherServiceImpl.java
@@ -1,36 +1,60 @@
 package com.ejournal.university.teacher.service.impl;
 
+import com.ejournal.university.common.exception.ResourceNotFoundException;
+import com.ejournal.university.department.entity.Department;
 import com.ejournal.university.teacher.dto.TeacherRequestDto;
 import com.ejournal.university.teacher.dto.TeacherResponseDto;
+import com.ejournal.university.teacher.entity.Teacher;
+import com.ejournal.university.teacher.mapper.TeacherMapper;
+import com.ejournal.university.teacher.repository.TeacherRepository;
 import com.ejournal.university.teacher.service.TeacherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class TeacherServiceImpl implements TeacherService {
+
+    private final TeacherRepository teacherRepository;
+
     @Override
     public TeacherResponseDto create(TeacherRequestDto requestDto) {
-        return null;
+        Teacher teacher = TeacherMapper.mapToEntity(requestDto, new Teacher());
+        return TeacherMapper.mapToDto(teacherRepository.createInstance(teacher));
     }
 
     @Override
     public TeacherResponseDto fetchById(Long id) {
-        return null;
+        Teacher teacher = teacherRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Teacher", "id", String.valueOf(id)));
+        return TeacherMapper.mapToDto(teacher);
     }
 
     @Override
     public List<TeacherResponseDto> fetchAll() {
-        return null;
+        List<Teacher> allTeachers = teacherRepository.fetchAllInstances();
+        return allTeachers.stream()
+                .map(TeacherMapper::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public TeacherResponseDto update(Long id, TeacherRequestDto requestDto) {
-        return null;
+        Teacher teacher = teacherRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Teacher", "id", String.valueOf(id)));
+        Teacher updatedTeacher = TeacherMapper.mapToEntity(requestDto, teacher);
+        updatedTeacher = teacherRepository.updateInstance(updatedTeacher);
+        return TeacherMapper.mapToDto(updatedTeacher);
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        Teacher teacher = teacherRepository.fetchInstanceById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Teacher", "id", String.valueOf(id)));
+        teacherRepository.deleteInstance(teacher);
+        return true;
     }
 }
