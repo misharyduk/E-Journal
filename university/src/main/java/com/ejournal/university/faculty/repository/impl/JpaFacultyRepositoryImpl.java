bc4c26742a77fb50dd697fcc431fded6544cb72c
misharyduk <misharyduk64@gmail.com>
2023-11-27 14:22:55 +0200

    added repository annotation to faculty JPA repo

--- university/src/main/java/com/ejournal/university/faculty/repository/impl/JpaFacultyRepositoryImpl.java
+++ university/src/main/java/com/ejournal/university/faculty/repository/impl/JpaFacultyRepositoryImpl.java
@@ -4,10 +4,12 @@ import com.ejournal.university.faculty.entity.Faculty;
 import com.ejournal.university.faculty.repository.FacultyRepository;
 import com.ejournal.university.teacher.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface JpaFacultyRepositoryImpl extends JpaRepository<Faculty, Long>, FacultyRepository {
     @Override
     default List<Faculty> fetchAllInstances(){
