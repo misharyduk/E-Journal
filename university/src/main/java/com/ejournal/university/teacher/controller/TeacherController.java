498e188adc383499fc75ad0df7d868775596dd1c
misharyduk <misharyduk64@gmail.com>
2023-11-27 13:44:33 +0200

    injected service to teacher's controller

--- university/src/main/java/com/ejournal/university/teacher/controller/TeacherController.java
+++ university/src/main/java/com/ejournal/university/teacher/controller/TeacherController.java
@@ -3,6 +3,8 @@ package com.ejournal.university.teacher.controller;
 import com.ejournal.university.common.dto.ResponseDto;
 import com.ejournal.university.teacher.dto.TeacherRequestDto;
 import com.ejournal.university.teacher.dto.TeacherResponseDto;
+import com.ejournal.university.teacher.service.TeacherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,46 +13,55 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/teachers")
+@RequiredArgsConstructor
 public class TeacherController {
 
-    @PostMapping
-    public ResponseEntity<ResponseDto> createTeacher(@RequestBody TeacherRequestDto teacherRequestDto){
+    private final TeacherService teacherService;
 
+    @PostMapping
+    public ResponseEntity<TeacherResponseDto> createTeacher(@RequestBody TeacherRequestDto teacherRequestDto){
+
+        TeacherResponseDto responseDto = teacherService.create(teacherRequestDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new ResponseDto(HttpStatus.CREATED.toString(), "Teacher has been successfully created"));
+                .body(responseDto);
     }
 
     @GetMapping
     public ResponseEntity<List<TeacherResponseDto>> fetchAllTeachers(){
 
+        List<TeacherResponseDto> responseDTOs = teacherService.fetchAll();
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(List.of());
+                .body(responseDTOs);
     }
 
     @GetMapping("/{teacherId}")
-    public ResponseEntity<TeacherResponseDto> fetchTeacher(@PathVariable("teacherId") Integer teacherId){
+    public ResponseEntity<TeacherResponseDto> fetchTeacher(@PathVariable("teacherId") Long teacherId){
 
+        TeacherResponseDto responseDto = teacherService.fetchById(teacherId);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new TeacherResponseDto());
+                .body(responseDto);
     }
 
-    @PutMapping
-    public ResponseEntity<TeacherResponseDto> updateTeacher(@RequestBody TeacherRequestDto teacherRequestDto){
+    @PutMapping("/{teacherId}")
+    public ResponseEntity<TeacherResponseDto> updateTeacher(@PathVariable("teacherId") Long teacherId,
+                                                            @RequestBody TeacherRequestDto teacherRequestDto){
 
+        TeacherResponseDto updatedResponseDto = teacherService.update(teacherId, teacherRequestDto);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new TeacherResponseDto());
+                .body(updatedResponseDto);
     }
 
     @DeleteMapping("/{teacherId}")
-    public ResponseEntity<ResponseDto> deleteTeacher(@PathVariable("teacherId") Integer teacherId){
+    public ResponseEntity<ResponseDto> deleteTeacher(@PathVariable("teacherId") Long teacherId){
 
+        teacherService.deleteById(teacherId);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ResponseDto(HttpStatus.OK.toString(), "Teacher has been successfully created"));
+                .body(new ResponseDto(HttpStatus.OK.toString(), "Teacher has been successfully deleted"));
     }
 
 }
--- university/src/main/java/com/ejournal/university/teacher/service/impl/TeacherServiceImpl.java
+++ university/src/main/java/com/ejournal/university/teacher/service/impl/TeacherServiceImpl.java
@@ -3,9 +3,11 @@ package com.ejournal.university.teacher.service.impl;
 import com.ejournal.university.teacher.dto.TeacherRequestDto;
 import com.ejournal.university.teacher.dto.TeacherResponseDto;
 import com.ejournal.university.teacher.service.TeacherService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TeacherServiceImpl implements TeacherService {
     @Override
     public TeacherResponseDto create(TeacherRequestDto requestDto) {
