54d15ba5c771bb4f7b4df6512b5f2d40be68be84
misharyduk <misharyduk64@gmail.com>
2023-11-28 22:12:43 +0200

    added json include for university request

--- university/src/main/java/com/ejournal/university/info/dto/UniversityRequestDto.java
+++ university/src/main/java/com/ejournal/university/info/dto/UniversityRequestDto.java
@@ -2,11 +2,13 @@ package com.ejournal.university.info.dto;
 
 import com.ejournal.university.common.dto.AddressDto;
 import com.ejournal.university.teacher.dto.TeacherRequestDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter @Setter
 public class UniversityRequestDto {
 
@@ -16,6 +18,7 @@ public class UniversityRequestDto {
     private String mobilePhone;
     private String email;
     private String accreditation;
+    private Long rectorId;
     private TeacherRequestDto rector;
 
     public UniversityRequestDto(){}
