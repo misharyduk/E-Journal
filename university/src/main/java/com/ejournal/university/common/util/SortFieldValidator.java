9fd5eff2758e5dda8e9b5f4c20c7b60229333eef
misharyduk <misharyduk64@gmail.com>
2023-11-29 17:47:29 +0200

    added validHQLField for all sorting

--- university/src/main/java/com/ejournal/university/common/util/SortFieldValidator.java
+++ university/src/main/java/com/ejournal/university/common/util/SortFieldValidator.java
@@ -3,6 +3,7 @@ package com.ejournal.university.common.util;
 import lombok.Getter;
 
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.Set;
 
 public class SortFieldValidator {
@@ -10,68 +11,103 @@ public class SortFieldValidator {
     @Getter
     public enum UniversityField{
 
-        ID(Set.of("id", "identifier", "number")),
-        NAME(Set.of("name", "title")),
-        ACCREDITATION(Set.of("accreditation", "acc")),
-        FACULTIES(Set.of("faculties", "faculty")),
-        DEPARTMENTS(Set.of("departments", "department")),
-        STUDENTS(Set.of("students", "student")),
-        TEACHERS(Set.of("teachers", "teacher", "personal"));
+        UNIVERSITY_ID(Set.of("id", "identifier", "number"), "id"),
+        UNIVERSITY_NAME(Set.of("name", "title"), "universityName"),
+        ACCREDITATION(Set.of("accreditation", "acc"), "accreditation"),
+        FACULTIES(Set.of("faculties", "faculty"), "faculties"),
+        DEPARTMENTS(Set.of("departments", "department"), "departments"),
+        STUDENTS(Set.of("students", "student"), "students"),
+        TEACHERS(Set.of("teachers", "teacher", "personal"), "teachers");
 
-        private UniversityField(Collection<String> fieldNameVariations){
+        private UniversityField(Collection<String> fieldNameVariations, String validHQLField){
             this.fieldNameVariations = fieldNameVariations;
+            this.validHQLField = validHQLField;
         }
 
         private final Collection<String> fieldNameVariations;
+        private final String validHQLField;
 
+        public static UniversityField validate(String inputFieldName){
+            EnumSet<UniversityField> enumSet = EnumSet.allOf(UniversityField.class);
+            return enumSet.stream()
+                    .filter(field -> field.getFieldNameVariations().contains(inputFieldName))
+                    .findFirst()
+                    .orElse(UNIVERSITY_ID);
+        }
     }
 
     @Getter
     public enum FacultyField{
 
-        ID(Set.of("id", "identifier", "number")),
-        NAME(Set.of("name", "title")),
-        DEPARTMENTS(Set.of("departments", "department")),
-        STUDENTS(Set.of("students", "student")),
-        TEACHERS(Set.of("teachers", "teacher", "personal"));
+        FACULTY_ID(Set.of("id", "identifier", "number"), "id"),
+        FACULTY_NAME(Set.of("name", "title"), "facultyName"),
+        DEPARTMENTS(Set.of("departments", "department"), "departments"),
+        STUDENTS(Set.of("students", "student"), "students"),
+        TEACHERS(Set.of("teachers", "teacher", "personal"), "teachers");
 
-        FacultyField(Collection<String> fieldNameVariations){
+        FacultyField(Collection<String> fieldNameVariations, String validHQLField){
             this.fieldNameVariations = fieldNameVariations;
+            this.validHQLField = validHQLField;
         }
 
         private final Collection<String> fieldNameVariations;
+        private final String validHQLField;
 
+        public static FacultyField validate(String inputFieldName){
+            EnumSet<FacultyField> enumSet = EnumSet.allOf(FacultyField.class);
+            return enumSet.stream()
+                    .filter(field -> field.getFieldNameVariations().contains(inputFieldName))
+                    .findFirst()
+                    .orElse(FACULTY_ID);
+        }
     }
 
     @Getter
     public enum DepartmentField{
 
-        ID(Set.of("id", "identifier", "number")),
-        NAME(Set.of("name", "title")),
-        STUDENTS(Set.of("students", "student"));
+        DEPARTMENT_ID(Set.of("id", "identifier", "number"), "id"),
+        DEPARTMENT_NAME(Set.of("name", "title"), "departmentName"),
+        STUDENTS(Set.of("students", "student"), "studentsAmount");
 
-        DepartmentField(Collection<String> fieldNameVariations){
+        DepartmentField(Collection<String> fieldNameVariations, String validHQLField){
             this.fieldNameVariations = fieldNameVariations;
+            this.validHQLField = validHQLField;
         }
 
         private final Collection<String> fieldNameVariations;
+        private final String validHQLField;
 
+        public static DepartmentField validate(String inputFieldName){
+            EnumSet<DepartmentField> enumSet = EnumSet.allOf(DepartmentField.class);
+            return enumSet.stream()
+                    .filter(field -> field.getFieldNameVariations().contains(inputFieldName))
+                    .findFirst()
+                    .orElse(DEPARTMENT_ID);
+        }
     }
 
     @Getter
     public enum TeacherField {
 
-        ID(Set.of("id", "identifier", "number")),
-        FIRST_NAME(Set.of("first_name", "firstName", "first")),
-        LAST_NAME(Set.of("last_name", "lastName", "last", "surname", "name")),
-        MIDDLE_NAME(Set.of("middle_name", "middleName", "middle"));
+        TEACHER_ID(Set.of("id", "identifier", "number"), "id"),
+        FIRST_NAME(Set.of("first_name", "firstName", "first"), "firstName"),
+        LAST_NAME(Set.of("last_name", "lastName", "last", "surname", "name"), "lastName");
 
-        TeacherField(Collection<String> fieldNameVariations){
+        TeacherField(Collection<String> fieldNameVariations, String validHQLField){
             this.fieldNameVariations = fieldNameVariations;
+            this.validHQLField = validHQLField;
         }
 
         private final Collection<String> fieldNameVariations;
+        private final String validHQLField;
 
+        public static TeacherField validate(String inputFieldName){
+            EnumSet<TeacherField> enumSet = EnumSet.allOf(TeacherField.class);
+            return enumSet.stream()
+                    .filter(field -> field.getFieldNameVariations().contains(inputFieldName))
+                    .findFirst()
+                    .orElse(TEACHER_ID);
+        }
         }
 
 }
