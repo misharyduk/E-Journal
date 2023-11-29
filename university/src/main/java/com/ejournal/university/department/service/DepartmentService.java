9f2f630b58ee334d59b88b71a8e084c3d43b5c28
misharyduk <misharyduk64@gmail.com>
2023-11-29 17:39:01 +0200

    added department service interface method for pagination and sorting

--- university/src/main/java/com/ejournal/university/department/service/DepartmentService.java
+++ university/src/main/java/com/ejournal/university/department/service/DepartmentService.java
@@ -1,5 +1,7 @@
 package com.ejournal.university.department.service;
 
+import com.ejournal.university.common.dto.PageableRequestDto;
+import com.ejournal.university.common.dto.PageableResponseDto;
 import com.ejournal.university.common.service.CommonCrudService;
 import com.ejournal.university.department.dto.DepartmentRequestDto;
 import com.ejournal.university.department.dto.DepartmentResponseDto;
@@ -7,5 +9,7 @@ import com.ejournal.university.department.dto.DepartmentResponseDto;
 import java.util.List;
 
 public interface DepartmentService extends CommonCrudService<DepartmentRequestDto, DepartmentResponseDto> {
+
+    PageableResponseDto<DepartmentResponseDto> fetchPage(PageableRequestDto pageableRequestDto);
 
 }
