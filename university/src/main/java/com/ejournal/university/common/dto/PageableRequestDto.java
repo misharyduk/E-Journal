11f06e1b1752e98aa01eaa8070be40e93e08dd97
misharyduk <misharyduk64@gmail.com>
2023-11-29 17:40:48 +0200

    fixed issue with nulls in page & size params

--- university/src/main/java/com/ejournal/university/common/dto/PageableRequestDto.java
+++ university/src/main/java/com/ejournal/university/common/dto/PageableRequestDto.java
@@ -1,19 +1,27 @@
 package com.ejournal.university.common.dto;
 
-public class PageableDto {
+import lombok.Setter;
+
+@Setter
+public class PageableRequestDto {
     private Integer page;
     private Integer size;
     private String field;
+    private String dir;
 
     public Integer getPage() {
-        return page > 0 ? page : 1;
+        return page != null && page > 0 ? page : 1;
     }
 
     public Integer getSize() {
-        return size > 0 ? size : 10;
+        return size != null && size > 0 ? size : 10;
     }
 
     public String getField() {
         return field != null && !field.isEmpty() && !field.isBlank() ? field : "id";
+    }
+
+    public String getDir() {
+        return dir != null && dir.equalsIgnoreCase("desc") ? dir : "asc";
     }
 }
