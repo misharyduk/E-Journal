0516c6741fad5e91f6cc0426c6b0335279111521
misharyduk <misharyduk64@gmail.com>
2023-12-01 20:44:17 +0200

    added page method for faculty service

--- university/src/main/java/com/ejournal/university/faculty/service/FacultyService.java
+++ university/src/main/java/com/ejournal/university/faculty/service/FacultyService.java
@@ -1,14 +1,13 @@
 package com.ejournal.university.faculty.service;
 
-import com.ejournal.university.common.dto.ResponseDto;
+import com.ejournal.university.common.dto.PageableRequestDto;
+import com.ejournal.university.common.dto.PageableResponseDto;
 import com.ejournal.university.common.service.CommonCrudService;
 import com.ejournal.university.faculty.dto.FacultyRequestDto;
 import com.ejournal.university.faculty.dto.FacultyResponseDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface FacultyService extends CommonCrudService<FacultyRequestDto, FacultyResponseDto> {
+    PageableResponseDto<FacultyResponseDto> fetchPage(PageableRequestDto pageableRequestDto);
 }
