76fc7bf4792d259048f5a1b97c027c1898af2116
misharyduk <misharyduk64@gmail.com>
2024-02-01 21:06:48 +0200

    remove unused imports

--- university/src/main/java/com/ejournal/university/faculty/repository/FacultyPaginationRepository.java
+++ university/src/main/java/com/ejournal/university/faculty/repository/FacultyPaginationRepository.java
@@ -1,12 +1,8 @@
 package com.ejournal.university.faculty.repository;
 
-import com.ejournal.university.faculty.entity.Faculty;
-import com.ejournal.university.faculty.repository.impl.FacultyPaginationRepositoryImpl;
 import jakarta.persistence.Tuple;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface FacultyPaginationRepository {
 
--- university/src/main/java/com/ejournal/university/faculty/repository/impl/FacultyPaginationRepositoryCriteriaImpl.java
+++ university/src/main/java/com/ejournal/university/faculty/repository/impl/FacultyPaginationRepositoryCriteriaImpl.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class FacultyPaginationRepositoryImpl implements FacultyPaginationRepository {
+public class FacultyPaginationRepositoryCriteriaImpl implements FacultyPaginationRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
