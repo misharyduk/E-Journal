0262ba1960778221a3b2b61ec90bbcd42d713e20
misharyduk <misharyduk64@gmail.com>
2024-04-08 17:15:52 +0300

    init group microservice

---
+++ group/src/test/java/com/ejournal/group/GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejournal.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
