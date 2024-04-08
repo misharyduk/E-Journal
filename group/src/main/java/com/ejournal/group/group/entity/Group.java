965dc4ae11e503d5db024428260741103cdcb1d7
misharyduk <misharyduk64@gmail.com>
2024-04-08 18:00:12 +0300

    init basic group entity

---
+++ group/src/main/java/com/ejournal/group/group/entity/Group.java
@@ -0,0 +1,22 @@
+package com.ejournal.group.group.entity;
+
+import com.ejournal.group.student.entity.Student;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Group {
+    @Id
+    @SequenceGenerator(name = "group_id_sequence_generator", sequenceName = "group_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "group_id_sequence_generator")
+    @Column(name = "group_id")
+    private Long id;
+    @Column(name = "group_number")
+    private Integer groupNumber;
+    @OneToMany(mappedBy = "group_id")
+    private List<Student> students;
+}
