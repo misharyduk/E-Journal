f7c5a6a09c738bbb3f4e1c562de03d0d990aecb6
misharyduk <misharyduk64@gmail.com>
2024-04-11 19:49:52 +0300

    init openfeign client

--- group/src/main/java/com/ejournal/group/GroupApplication.java
+++ group/src/main/java/com/ejournal/group/GroupApplication.java
@@ -2,8 +2,10 @@ package com.ejournal.group;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GroupApplication {
 
     public static void main(String[] args) {
--- university/src/main/java/com/ejournal/university/UniversityApplication.java
+++ university/src/main/java/com/ejournal/university/UniversityApplication.java
@@ -2,8 +2,10 @@ package com.ejournal.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UniversityApplication {
 
 	public static void main(String[] args) {
