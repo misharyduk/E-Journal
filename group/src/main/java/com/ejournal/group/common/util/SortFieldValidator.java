7bcaf5db9268a459ff3ed736d096d09be51c43af
misharyduk <misharyduk64@gmail.com>
2024-04-11 17:16:50 +0300

    fix bug with sorting

--- group/src/main/java/com/ejournal/group/common/util/SortFieldValidator.java
+++ group/src/main/java/com/ejournal/group/common/util/SortFieldValidator.java
@@ -11,7 +11,7 @@ public class SortFieldValidator {
     @Getter
     public enum GroupField{
 
-        GROUP_ID(Set.of("id", "identifier", "number"), "id"),
+        GROUP_ID(Set.of("id", "identifier"), "id"),
         GROUP_NUMBER(Set.of("number", "group", "name", "group_number", "group_name"), "groupNumber"),
         STUDENTS(Set.of("students", "student"), "students");
 
