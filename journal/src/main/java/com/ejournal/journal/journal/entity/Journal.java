3b487b3c01e9f7cc5c56a959ee1c0790f9213d64
misharyduk <misharyduk64@gmail.com>
2024-04-13 18:49:13 +0300

    init basic entity

--- journal/src/main/java/com/ejournal/journal/journal/entity/Journal.java
+++ journal/src/main/java/com/ejournal/journal/journal/entity/Journal.java
@@ -1,11 +1,11 @@
 package com.ejournal.journal.journal.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Journal {
     @Id
     @SequenceGenerator(name = "journal_id_sequence_generator", sequenceName = "journal_id_sequence", allocationSize = 1)
@@ -18,4 +18,10 @@ public class Journal {
     private Long groupId;
     @Column(name = "teacher_id")
     private Long teacherId;
+
+    public Journal(Long subjectId, Long groupId, Long teacherId) {
+        this.subjectId = subjectId;
+        this.groupId = groupId;
+        this.teacherId = teacherId;
+    }
 }
