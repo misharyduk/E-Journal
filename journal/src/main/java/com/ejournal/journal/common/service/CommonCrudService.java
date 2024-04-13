0cea0187131b45b2a2084d308f204981718e48bf
misharyduk <misharyduk64@gmail.com>
2024-04-13 18:48:45 +0300

    delete redundant methods

--- journal/src/main/java/com/ejournal/journal/common/service/CommonCrudService.java
+++ journal/src/main/java/com/ejournal/journal/common/service/CommonCrudService.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 public interface CommonCrudService<K1, K2> {
 
-    K2 create(K1 requestDto);
+    Long create(K1 requestDto);
 
     K2 fetchById(Long id);
 
     List<K2> fetchAll();
-
-    K2 update(Long id, K1 requestDto);
 
     boolean deleteById(Long id);
 
