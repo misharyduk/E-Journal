40764399d6878c8033129102c11e956fa823d9b8
misharyduk <misharyduk64@gmail.com>
2024-04-14 16:12:18 +0300

    added lessons mapping to DTO

--- journal/src/main/java/com/ejournal/journal/journal/dto/JournalResponseDto.java
+++ journal/src/main/java/com/ejournal/journal/journal/dto/JournalResponseDto.java
@@ -1,8 +1,10 @@
 package com.ejournal.journal.journal.dto;
 
+import com.ejournal.journal.common.dto.PageableResponseDto;
 import com.ejournal.journal.common.feign_client.group.dto.GroupResponseDto;
 import com.ejournal.journal.common.feign_client.university.dto.SubjectResponseDto;
 import com.ejournal.journal.common.feign_client.university.dto.TeacherResponseDto;
+import com.ejournal.journal.lesson.dto.LessonResponseDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +14,5 @@ public class JournalResponseDto {
     private SubjectResponseDto subject;
     private GroupResponseDto group;
     private TeacherResponseDto teacher;
+    private PageableResponseDto<LessonResponseDto> lessons;
 }
