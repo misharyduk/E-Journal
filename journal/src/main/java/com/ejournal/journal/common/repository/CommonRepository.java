26f94eab5b8a2d1d102750618a757a34204ae323
misharyduk <misharyduk64@gmail.com>
2024-04-14 16:06:29 +0300

    remove uncommon methods

--- journal/src/main/java/com/ejournal/journal/common/repository/CommonRepository.java
+++ journal/src/main/java/com/ejournal/journal/common/repository/CommonRepository.java
@@ -1,5 +1,8 @@
 package com.ejournal.journal.common.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +11,8 @@ public interface CommonRepository<K> {
     List<K> fetchAllInstances();
 
     Optional<K> fetchInstanceById(Long id);
+
+    Page<K> fetchPage(Pageable pageable);
 
     K createInstance(K instance);
 
