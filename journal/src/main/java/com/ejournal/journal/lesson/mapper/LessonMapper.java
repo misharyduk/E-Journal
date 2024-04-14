c3bed959ac2e9f499530bb3fdd4b113b6fd27947
misharyduk <misharyduk64@gmail.com>
2024-04-14 16:09:23 +0300

    added lesson basic mapper

---
+++ journal/src/main/java/com/ejournal/journal/lesson/mapper/LessonMapper.java
@@ -0,0 +1,26 @@
+package com.ejournal.journal.lesson.mapper;
+
+import com.ejournal.journal.lesson.dto.LessonRequestDto;
+import com.ejournal.journal.lesson.dto.LessonResponseDto;
+import com.ejournal.journal.lesson.entity.Lesson;
+
+public class LessonMapper {
+
+    public static Lesson mapToEntity(LessonRequestDto lessonRequestDto, Lesson lesson){
+        lesson.setType(lessonRequestDto.getType());
+        lesson.setDate(lessonRequestDto.getDate());
+        lesson.setOrder(lessonRequestDto.getOrder());
+        lesson.setAuditory(lessonRequestDto.getAuditory());
+        return lesson;
+    }
+
+    public static LessonResponseDto mapToDto(Lesson lesson, LessonResponseDto lessonResponseDto){
+        lessonResponseDto.setType(lesson.getType());
+        lessonResponseDto.setDate(lesson.getDate());
+        lessonResponseDto.setOrder(lesson.getOrder());
+        lessonResponseDto.setAuditory(lesson.getAuditory());
+        lessonResponseDto.setJournalId(lesson.getJournal().getId());
+        return lessonResponseDto;
+    }
+
+}
