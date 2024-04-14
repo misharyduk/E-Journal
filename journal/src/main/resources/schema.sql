14a5409401b26beab93f085be4724ea94cf0e4f0
misharyduk <misharyduk64@gmail.com>
2024-04-14 17:50:11 +0300

    added lesson sql schema

--- journal/src/main/resources/schema.sql
+++ journal/src/main/resources/schema.sql
@@ -1,4 +1,5 @@
 CREATE SEQUENCE journal_id_sequence INCREMENT BY 1;
+CREATE SEQUENCE lesson_id_sequence INCREMENT BY 1;
 
 CREATE TABLE IF NOT EXISTS journal(
     journal_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -7,4 +8,13 @@ CREATE TABLE IF NOT EXISTS journal(
     teacher_id BIGINT NOT NULL
 );
 
--- ALTER TABLE student ADD FOREIGN KEY (group_id) REFERENCES app_group(group_id) ON DELETE SET NULL;
+CREATE TABLE IF NOT EXISTS lesson(
+    lesson_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lesson_type VARCHAR(100) NOT NULL,
+    lesson_date TIMESTAMP NOT NULL,
+    lesson_order INT NOT NULL,
+    auditory VARCHAR(50) NOT NULL,
+    journal_id BIGINT
+);
+
+ALTER TABLE lesson ADD FOREIGN KEY (journal_id) REFERENCES journal(journal_id) ON DELETE CASCADE;
