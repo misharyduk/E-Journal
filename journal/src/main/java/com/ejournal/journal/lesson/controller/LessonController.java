39c1a96203e19867f184913fda49d0650e7d2f88
misharyduk <misharyduk64@gmail.com>
2024-04-14 16:11:01 +0300

    added lesson's  controller impl with CRUD

---
+++ journal/src/main/java/com/ejournal/journal/lesson/controller/LessonController.java
@@ -0,0 +1,61 @@
+package com.ejournal.journal.lesson.controller;
+
+import com.ejournal.journal.common.dto.ResponseDto;
+import com.ejournal.journal.journal.dto.JournalRequestDto;
+import com.ejournal.journal.journal.dto.JournalResponseDto;
+import com.ejournal.journal.lesson.dto.LessonRequestDto;
+import com.ejournal.journal.lesson.dto.LessonResponseDto;
+import com.ejournal.journal.lesson.service.LessonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.ejournal.journal.common.util.ServiceConstants.INSTANCE_DELETED;
+
+@RestController
+@RequestMapping("/api/v1/lessons")
+@RequiredArgsConstructor
+public class LessonController {
+
+    private final LessonService lessonService;
+
+    @PostMapping
+    public ResponseEntity<LessonResponseDto> createLesson(@RequestBody LessonRequestDto lessonRequestDto){
+
+        LessonResponseDto lessonResponseDto = lessonService.create(lessonRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(lessonResponseDto);
+    }
+
+    @GetMapping("/{lessonId}")
+    public ResponseEntity<LessonResponseDto> fetchLesson(@PathVariable("lessonId") Long lessonId){
+
+        LessonResponseDto responseDto = lessonService.fetchById(lessonId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseDto);
+    }
+
+    @PutMapping("/{lessonId}")
+    public ResponseEntity<LessonResponseDto> updateLesson(@PathVariable("lessonId") Long lessonId,
+                                                        @RequestBody LessonRequestDto lessonRequestDto){
+
+        LessonResponseDto updatedResponseDto = lessonService.update(lessonId, lessonRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(updatedResponseDto);
+    }
+
+    @DeleteMapping("/{lessonId}")
+    public ResponseEntity<ResponseDto> deleteLesson(@PathVariable("lessonId") Long lessonId){
+
+        lessonService.deleteById(lessonId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(HttpStatus.OK.toString(), "Lesson" + INSTANCE_DELETED));
+    }
+}
