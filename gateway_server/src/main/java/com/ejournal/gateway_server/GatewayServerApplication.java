f6408358b6482c7047289beb5996780bef6d13da
misharyduk <misharyduk64@gmail.com>
2024-05-11 19:30:57 +0300

    configure routes

--- gateway_server/src/main/java/com/ejournal/gateway_server/GatewayServerApplication.java
+++ gateway_server/src/main/java/com/ejournal/gateway_server/GatewayServerApplication.java
@@ -2,6 +2,9 @@ package com.ejournal.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -10,4 +13,23 @@ public class GatewayServerApplication {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator systemRouteConfiguration(RouteLocatorBuilder routeLocatorConfig){
+        return routeLocatorConfig.routes()
+                .route(r -> r
+                        .path("/university/**")
+                        .filters(f -> f.rewritePath("/university/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://UNIVERSITY")
+                )
+                .route(r -> r
+                        .path("/group/**")
+                        .filters(f -> f.rewritePath("/group/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://GROUP")
+                )
+                .route(r -> r
+                        .path("/journal/**")
+                        .filters(f -> f.rewritePath("/journal/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://JOURNAL")
+                ).build();
+    }
 }
