3e8cb2c05017ed2e3113a5133dd1be5e8291af62
misharyduk <misharyduk64@gmail.com>
2024-05-22 17:49:48 +0300

    init commit of calendar plan microservice

---
+++ calendar_plan/src/main/java/com/ejournal/calendar_plan/CalendarPlanApplication.java
@@ -0,0 +1,13 @@
+package com.ejournal.calendar_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarPlanApplication.class, args);
+    }
+
+}
---
+++ calendar_plan/src/test/java/com/ejournal/calendar_plan/CalendarPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejournal.calendar_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
