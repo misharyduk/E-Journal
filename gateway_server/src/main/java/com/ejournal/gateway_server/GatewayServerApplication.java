5083d2b965e265fb6e49c42c5ff55c4d2846e194
misharyduk <misharyduk64@gmail.com>
2024-05-26 15:04:37 +0300

    added circuit breaker for microservices

--- gateway_server/src/main/java/com/ejournal/gateway_server/GatewayServerApplication.java
+++ gateway_server/src/main/java/com/ejournal/gateway_server/GatewayServerApplication.java
@@ -18,25 +18,35 @@ public class GatewayServerApplication {
         return routeLocatorConfig.routes()
                 .route(r -> r
                         .path("/university/**")
-                        .filters(f -> f.rewritePath("/university/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/university/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(circBreakConfig -> circBreakConfig
+                                        .setName("universityCircuitBreaker")))
                         .uri("lb://UNIVERSITY")
                 )
                 .route(r -> r
                         .path("/group/**")
-                        .filters(f -> f.rewritePath("/group/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/group/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(circBreakConfig -> circBreakConfig
+                                        .setName("groupCircuitBreaker")))
                         .uri("lb://GROUP")
                 )
                 .route(r -> r
                         .path("/journal/**")
-                        .filters(f -> f.rewritePath("/journal/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/journal/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(circBreakConfig -> circBreakConfig
+                                        .setName("journalCircuitBreaker")))
                         .uri("lb://JOURNAL")
                 ).route(r -> r
                         .path("/analytics/**")
-                        .filters(f -> f.rewritePath("/analytics/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/analytics/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(circBreakConfig -> circBreakConfig
+                                        .setName("analyticsCircuitBreaker")))
                         .uri("lb://ANALYTICS")
                 ).route(r -> r
                         .path("/calendarplan/**")
-                        .filters(f -> f.rewritePath("/calendarplan/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/calendarplan/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(circBreakConfig -> circBreakConfig
+                                        .setName("calendarPlanCircuitBreaker")))
                         .uri("lb://CALENDARPLAN")
                 )
                 .build();
